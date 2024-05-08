dec316e4fc63b30d126c591727b681e6464da719
clued1001 <167940038+clued1001@users.noreply.github.com>
2024-05-08 15:27:42 +0900

    버전 1.0

--- home.py
+++ home.py
@@ -7,3 +7,6 @@ def run_home():
     
     url = 'https://cdn.safetimes.co.kr/news/photo/202106/96020_76507_2517.jpg'
     st.image(url, use_column_width= True)
+    
+    st.text('')
+    st.text('출처 : https://data.incheon.go.kr/findData/publicDataDetail?dataId=15048264&srcSe=7661IVAWM27C61E190')
